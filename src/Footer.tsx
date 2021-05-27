9fdc506db4de3d9a77e32e30e469a42b6ae5c425
Egorych <mikhalevichegor@gmail.com>
2021-05-27 17:38:07 +0300

    Component Footer.tsx

---
+++ src/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Footer() {
+    return (
+        <div className="footer">
+            <button>All</button>
+            <button>Completed</button>
+            <button>Active</button>
+        </div>
+    )
+}
