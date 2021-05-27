43c8a00cf43a0f113c7dd7bd03fa06efba637af0
Egorych <mikhalevichegor@gmail.com>
2021-05-27 16:41:46 +0300

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <div className="todoList">
+          <div className="todoList-header">
+            <h3 className="todoList-header__title">What to Learn</h3>
+            <div className="todoList-newTaskForm">
+              <input type="text" placeholder="title"/>
+              <button>Add</button>
+            </div>
+          </div>
+          <div className="todoList-tasks">
+            <div className="todoList-task">
+              <input type="checkbox" checked={true}/>
+              <span>CSS</span>
+            </div>
+            <div className="todoList-task">
+              <input type="checkbox" checked={false}/>
+              <span>JS</span>
+            </div>
+            <div className="todoList-task">
+              <input type="checkbox" checked={false}/>
+              <span>ReactJS</span>
+            </div>
+            <div className="todoList-task">
+              <input type="checkbox" checked={true}/>
+              <span>Patterns</span>
+            </div>
+          </div>
+          <div className="todoList-footer">
+            <button>All</button>
+            <button>Completed</button>
+            <button>Active</button>
+          </div>
+        </div>
+      </div>
   );
 }
 
