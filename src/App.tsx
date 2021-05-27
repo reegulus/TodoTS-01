57eff1ab647b670cfd04046457035985f6529e6f
Egorych <mikhalevichegor@gmail.com>
2021-05-27 17:50:44 +0300

    Component Todolist.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./Header";
 import {Todolist} from "./Todolist";
-import {Footer} from "./Footer";
 
 function App() {
     return (
         <div className="App">
             <div className="todoList">
-                <Header/>
                 <Todolist/>
-                <Footer/>
             </div>
         </div>
     );
